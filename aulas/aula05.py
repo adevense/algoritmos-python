8c1f10a82ce9e7843c9bb56aa80f1ed8299b3f6d
adevense <adevense016@gmail.com>
2025-05-06 21:10:02 -0400

    aula5

---
+++ aulas/aula05.py
@@ -0,0 +1,28 @@
+'''
+Lista/Tuplas/dicionario
+
+Listas -> coleções heterogeneas de objetos, podem ser de qualquer tipo, inclusive outras listas
+[0, 1, 10, "Beto",[2,5]]
+
+listas são mutaveis
+
+lista01=[1,2,3,4,5,6,7,8,9,10]
+
+lista02=[3.14,'beto',True,[]]
+'''
+
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+print(lista)
+
+nome = "Beto"
+
+print(nome[0])
+
+#len -> é uma função que retorna o tamanho de uma coleção
+print(len(nome))
+
+progs=['Yes','Genesis','Pink Floyd','ELP']
+
+print(progs[0])
+print(progs[1])
--- aulas/aula_01.py
+++ aulas/aula_01.py
@@ -14,3 +14,5 @@ print('A média é: ',(nota_01 + nota_02 + nota_03 + nota_04)/4)
 #ler 4 notas e fazer media ponderada
 print('A média ponderada é: ',(nota_01*0.2 + nota_02*0.1 + nota_03*0.4 + nota_04*0.3))
 print(f'nota 1: {nota_01*0.2} nota 2: {nota_02*0.1} nota 3: {nota_03*0.4} nota 4: {nota_04*0.3}')
+
+#shift + alt + seta para baixo, clona a linha
